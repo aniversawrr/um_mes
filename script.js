725dcd5e498850504905479366cb76496e6d1a36
aniversawrr <104662179+aniversawrr@users.noreply.github.com>
2022-04-29 23:08:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+import Countdown from "./countdown.js";
+
+const tempoParaOWWB = new Countdown("01 May 2022 00:00:00 GMT-0300");
+const tempos = document.querySelectorAll("[data-time]");
+
+function mostrarTempo() {
+  tempos.forEach((tempo, index) => {
+    tempo.innerHTML = tempoParaOWWB.total[index];
+  });
+}
+mostrarTempo();
+setInterval(mostrarTempo, 1000);
+
+if (total == 0) {
+  window.location.href = "presente.html"
+}
